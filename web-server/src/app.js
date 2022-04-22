81cd73482faf877e2a7306e453904589ceae5aee
fport <furkanportakalx@gmail.com>
2022-04-22 09:35:22 +0300

    feat: add router

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,22 @@ app.get('/', (req, res) => {
     res.send('welcome')
 })
 
+app.get('/test_text', (req, res) => {
+    res.send("Öğrenci numarası => 18360859046")
+})
+
+app.get('/test_html', (req, res) => {
+    res.send('<h1>Muhammet Furkan Portakal</h1>')
+})
+
+app.get('/test_json', (req, res) => {
+    res.send({
+        "OgrenciNo":"18360859046",
+        "Adi":"Muhammet Furkan",
+        "Soyadi":"Portakal"
+    }).json()
+})
+
 app.listen(PORT, () => {
     console.log(`Application running on => ${PORT}`)
 })
