299084bec401828d5c310e259b2da8968bed0306
fport <furkanportakalx@gmail.com>
2022-04-22 10:12:06 +0300

    feat: ready geocode route

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -4,16 +4,28 @@ const axios = require('axios')
 const getGeocode = async (url) => {
     try {
         return await axios.get(url).then((req, res) => {
-            return req.data
+            return req.data.features[0].center
         }).catch(error => console.log('Error Handler running, this error =>', error))
     } catch (error) {
         console.log('Error Handler running, this error =>', error)
     }
 }
 
-const geocode = (city, callback) => {
-    console.log('city',city);
-    return callback(undefined, 'osman')
+const geocode = (address, callback) => {
+    const accessKey = process.env.API_KEY;
+    const url = `http://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${accessKey}`
+
+    getGeocode(url).then(res => {
+        if(res) {
+            const info = {
+                "enlem": res[0],
+                "boylam": res[1]
+            }
+           return callback(undefined, info) 
+        } else {
+            return callback('Error Handler running', undefined)
+        }
+    }).catch( error =>  console.log('Error Handler running, this error =>', error))
 }
 
 module.exports = geocode
